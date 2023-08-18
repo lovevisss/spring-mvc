d32c566d26fa8c51e9e92f7ee38ba3aedcb3834a
zsy <530822987@qq.com>
2023-08-18 09:42:18 +0800

    first controller

---
+++ new-learn-path-springmvc/src/main/java/com/zufedfc/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.zufedfc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    // "/" --> /WEB-INF/thymeleaf/index.html
+    @RequestMapping ("/")
+    public String index(){
+        //返回视图名称
+        return "index";
+    }
+
+    @RequestMapping ("/test")
+    public String hello(){
+        //返回视图名称
+        return "test";
+    }
+
+}
