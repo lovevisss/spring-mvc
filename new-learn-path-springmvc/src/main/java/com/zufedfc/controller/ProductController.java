277db287b85e2c8e77bedd2da535563fbc6c0a98
zsy <530822987@qq.com>
2023-08-18 16:09:27 +0800

    PUT POST 测试

--- new-learn-path-springmvc/src/main/java/com/zufedfc/controller/ProductController.java
+++ new-learn-path-springmvc/src/main/java/com/zufedfc/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.zufedfc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/product")
@@ -13,4 +16,26 @@ public class ProductController {
         //返回视图名称
         return "product/list";
     }
+
+    @RequestMapping(
+            value = {"/add", "/addone"}
+    )
+    public String add(){
+        //返回视图名称
+        return "product/add";
+    }
+
+    @RequestMapping(value = "/update" , method = {RequestMethod.PUT})
+    public String update(){
+        //返回视图名称
+        return "product/update";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "product/create";
+    }
+
+
+
 }
