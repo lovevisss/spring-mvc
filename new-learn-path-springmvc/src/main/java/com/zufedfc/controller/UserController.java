41f440ed22be1799879bbe3d2bf1939bfbcf6b33
zsy <530822987@qq.com>
2023-08-22 13:14:25 +0800

    request params

--- new-learn-path-springmvc/src/main/java/com/zufedfc/controller/UserController.java
+++ new-learn-path-springmvc/src/main/java/com/zufedfc/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.zufedfc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/user")
@@ -12,6 +19,37 @@ public class UserController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping("/delete/{id}/{name}")
+    public String delete(@PathVariable("id") Integer id, @PathVariable("name") String name){
+        System.out.println("delete id = " + id + ",name = " + name);
+        return "user/index";
+    }
+
+    @RequestMapping("/testServlet")
+    public String testServlet(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String name = request.getParameter("name");
+        String id = request.getParameter("id");
+
+        System.out.println("testServlet"+"name = " + name + ",id = " + id);
+        return "user/index";
+    }
+
+    @RequestMapping("/testForward")
+    public String testForward(
+            @RequestParam(value = "Name", required = false, defaultValue = "xiaoming") String name,
+            String id,
+            String[] hobby,
+            @RequestHeader(value = "User-Agent", required = false, defaultValue = "chrome") String userAgent
+    ){
+        System.out.println("testForward"+"name = " + name + ",id = " + id + ",hobby = " + Arrays.toString(hobby) + ",userAgent = " + userAgent );
+        return "user/index";
+    }
+
+
+
+
 
 
 }
