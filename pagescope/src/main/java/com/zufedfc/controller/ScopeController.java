c469d776f7ed645fea5d1f47efcede90d5dffbee
zsy <530822987@qq.com>
2023-08-25 09:35:52 +0800

    add session and application context scope

--- pagescope/src/main/java/com/zufedfc/controller/ScopeController.java
+++ pagescope/src/main/java/com/zufedfc/controller/ScopeController.java
@@ -2,6 +2,7 @@ package com.zufedfc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -40,6 +41,27 @@ public class ScopeController {
     public String MapApi(Map<String , Object> map){
 //        使用Map 向request域中存值
         map.put("requestScope","textMap");
+        return "test";
+    }
+
+    @RequestMapping("/testRequestByModelMap")
+    public String ModelMapApi(ModelMap model){
+//        使用ModelMap 向request域中存值
+        model.addAttribute("requestScope","textModelMap");
+        return "test";
+    }
+
+    @RequestMapping("/testSessionByServlet")
+    public String testSessionByServlet(HttpServletRequest request){
+//        使用servlet API 向session域中存值
+        request.getSession().setAttribute("sessionScope","textSessionScope");
+        return "test";
+    }
+
+    @RequestMapping("/testApplicationByServlet")
+    public String testApplicationByServlet(HttpServletRequest request){
+//        使用servlet API 向application域中存值
+        request.getSession().getServletContext().setAttribute("applicationScope","textApplicationScope");
         return "test";
     }
 
